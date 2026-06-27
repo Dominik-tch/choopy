9aa124c27452ce0c8cfb969e99cc461099c0ae4e
dominik.tch <dominik.tasch04@gmail.com>
2026-06-27 16:14:35 +0200

    unziped spring initialization

---
+++ backend/choopy/src/main/java/com/taschion/choopy/ChoopyApplication.java
@@ -0,0 +1,13 @@
+package com.taschion.choopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoopyApplication.class, args);
+	}
+
+}
---
+++ backend/choopy/src/test/java/com/taschion/choopy/ChoopyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taschion.choopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
